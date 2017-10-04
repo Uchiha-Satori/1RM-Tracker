affe8c1b687c8b6c1a5e80d20a3cddab1905b92c
Uchiha-Satori <stephen.kit.ho@gmail.com>
2017-10-04 15:27:38 -0400

    Assets directory update
    Index.js update.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+// const ui = require('./auth/ui.js') // may not need
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +13,23 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+const recordEvents = require('../scripts/auth/events.js')
+
+$(() => {
+  $('#sign-up').on('submit', recordEvents.onSignUp)
+  $('#sign-in').on('submit', recordEvents.onSignIn)
+  $('#change-password').on('submit', recordEvents.onChangePassword)
+  $('#log-out').on('click', recordEvents.onLogOut)
+  $('#submit-record').on('click', recordEvents.onSubmitBlog)
+})
+// update button
+$(document).on('click', '.btnDeleteRecord', recordEvents.onBlogDelete)
+// edit button
+$(document).on('click', '#recordUpdateButton', recordEvents.onBlogEdit)
+
+$(document).on('show.bs.modal', (e) => {
+  let recordId = e.relatedTarget.dataset.id
+  console.log(recordId)
+  $('.btnUpdateRecord').attr('data-id', `${recordId}`)
+})
